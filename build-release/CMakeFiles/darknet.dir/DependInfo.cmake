15f99848c8e4c363f2c54fe7020453d402bdf1ea
vicent <vicent@ubuntu.lan>
2020-05-09 09:09:34 +0800

    first commit

---
+++ build-release/CMakeFiles/darknet.dir/DependInfo.cmake
@@ -0,0 +1,151 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CUDA"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/vicent/gittmp/darknet/src/activation_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/activation_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/activations.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/activations.c.o"
+  "/home/vicent/gittmp/darknet/src/art.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/art.c.o"
+  "/home/vicent/gittmp/darknet/src/avgpool_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/avgpool_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/batchnorm_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/batchnorm_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/blas.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/blas.c.o"
+  "/home/vicent/gittmp/darknet/src/box.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/box.c.o"
+  "/home/vicent/gittmp/darknet/src/captcha.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/captcha.c.o"
+  "/home/vicent/gittmp/darknet/src/cifar.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/cifar.c.o"
+  "/home/vicent/gittmp/darknet/src/classifier.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/classifier.c.o"
+  "/home/vicent/gittmp/darknet/src/coco.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/coco.c.o"
+  "/home/vicent/gittmp/darknet/src/col2im.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/col2im.c.o"
+  "/home/vicent/gittmp/darknet/src/compare.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/compare.c.o"
+  "/home/vicent/gittmp/darknet/src/connected_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/connected_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/conv_lstm_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/conv_lstm_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/convolutional_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/convolutional_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/cost_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/cost_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/cpu_gemm.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/cpu_gemm.c.o"
+  "/home/vicent/gittmp/darknet/src/crnn_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/crnn_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/crop_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/crop_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/dark_cuda.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/dark_cuda.c.o"
+  "/home/vicent/gittmp/darknet/src/darknet.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/darknet.c.o"
+  "/home/vicent/gittmp/darknet/src/data.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/data.c.o"
+  "/home/vicent/gittmp/darknet/src/deconvolutional_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/deconvolutional_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/demo.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/demo.c.o"
+  "/home/vicent/gittmp/darknet/src/detection_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/detection_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/detector.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/detector.c.o"
+  "/home/vicent/gittmp/darknet/src/dice.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/dice.c.o"
+  "/home/vicent/gittmp/darknet/src/dropout_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/dropout_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/gaussian_yolo_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/gaussian_yolo_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/gemm.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/gemm.c.o"
+  "/home/vicent/gittmp/darknet/src/go.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/go.c.o"
+  "/home/vicent/gittmp/darknet/src/gru_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/gru_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/im2col.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/im2col.c.o"
+  "/home/vicent/gittmp/darknet/src/image.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/image.c.o"
+  "/home/vicent/gittmp/darknet/src/layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/layer.c.o"
+  "/home/vicent/gittmp/darknet/src/list.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/list.c.o"
+  "/home/vicent/gittmp/darknet/src/local_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/local_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/lstm_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/lstm_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/matrix.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/matrix.c.o"
+  "/home/vicent/gittmp/darknet/src/maxpool_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/maxpool_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/network.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/network.c.o"
+  "/home/vicent/gittmp/darknet/src/nightmare.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/nightmare.c.o"
+  "/home/vicent/gittmp/darknet/src/normalization_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/normalization_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/option_list.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/option_list.c.o"
+  "/home/vicent/gittmp/darknet/src/parser.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/parser.c.o"
+  "/home/vicent/gittmp/darknet/src/region_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/region_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/reorg_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/reorg_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/reorg_old_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/reorg_old_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/rnn.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/rnn.c.o"
+  "/home/vicent/gittmp/darknet/src/rnn_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/rnn_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/rnn_vid.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/rnn_vid.c.o"
+  "/home/vicent/gittmp/darknet/src/route_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/route_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/sam_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/sam_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/scale_channels_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/scale_channels_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/shortcut_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/shortcut_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/softmax_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/softmax_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/super.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/super.c.o"
+  "/home/vicent/gittmp/darknet/src/swag.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/swag.c.o"
+  "/home/vicent/gittmp/darknet/src/tag.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/tag.c.o"
+  "/home/vicent/gittmp/darknet/src/tree.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/tree.c.o"
+  "/home/vicent/gittmp/darknet/src/upsample_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/upsample_layer.c.o"
+  "/home/vicent/gittmp/darknet/src/utils.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/utils.c.o"
+  "/home/vicent/gittmp/darknet/src/voxel.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/voxel.c.o"
+  "/home/vicent/gittmp/darknet/src/writing.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/writing.c.o"
+  "/home/vicent/gittmp/darknet/src/yolo.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/yolo.c.o"
+  "/home/vicent/gittmp/darknet/src/yolo_layer.c" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/yolo_layer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CUDNN"
+  "GPU"
+  "OPENCV"
+  "USE_CMAKE_LIBS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../3rdparty/stb/include"
+  "/usr/include/opencv4"
+  )
+set(CMAKE_DEPENDS_CHECK_CUDA
+  "/home/vicent/gittmp/darknet/src/activation_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/activation_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/avgpool_layer_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/avgpool_layer_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/blas_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/blas_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/col2im_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/col2im_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/convolutional_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/convolutional_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/crop_layer_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/crop_layer_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/deconvolutional_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/deconvolutional_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/dropout_layer_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/dropout_layer_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/im2col_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/im2col_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/maxpool_layer_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/maxpool_layer_kernels.cu.o"
+  "/home/vicent/gittmp/darknet/src/network_kernels.cu" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/network_kernels.cu.o"
+  )
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CUDA
+  "CUDNN"
+  "GPU"
+  "OPENCV"
+  "USE_CMAKE_LIBS"
+  )
+
+# The include file search paths:
+set(CMAKE_CUDA_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../3rdparty/stb/include"
+  "/usr/include/opencv4"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vicent/gittmp/darknet/src/http_stream.cpp" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/http_stream.cpp.o"
+  "/home/vicent/gittmp/darknet/src/image_opencv.cpp" "/home/vicent/gittmp/darknet/build-release/CMakeFiles/darknet.dir/src/image_opencv.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CUDNN"
+  "GPU"
+  "OPENCV"
+  "USE_CMAKE_LIBS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../3rdparty/stb/include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- net_cam_v3.sh
+++ net_cam_v3.sh
@@ -1,6 +1,7 @@
 #rm test_dnn_out.avi
 
-./darknet detector demo ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights rtsp://admin:admin12345@192.168.0.228:554 -i 0 -thresh 0.25
+#./darknet detector demo ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights rtsp://admin:admin12345@192.168.0.228:554 -i 0 -thresh 0.25
+./darknet detector demo ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights rtsp://admin:Ab12345678@172.17.1.99:554 -i 0 -thresh 0.25
 
 
 
