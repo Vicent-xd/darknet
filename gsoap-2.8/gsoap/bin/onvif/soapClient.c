1a2a6c844df97ec777295953f0e138b6884d7c34
xavier <t.vicent@qq.com>
2020-07-28 23:34:42 -0400

    compile sucess on xavier and makeclean

--- gsoap-2.8/gsoap/bin/onvif/soapClient.c
+++ gsoap-2.8/gsoap/bin/onvif/soapClient.c
@@ -17,7 +17,7 @@ compiling, linking, and/or using OpenSSL is allowed.
 extern "C" {
 #endif
 
-SOAP_SOURCE_STAMP("@(#) soapClient.c ver 2.8.22 2020-07-24 23:11:56 GMT")
+SOAP_SOURCE_STAMP("@(#) soapClient.c ver 2.8.22 2020-07-29 00:44:48 GMT")
 
 
 SOAP_FMAC5 int SOAP_FMAC6 soap_send_SOAP_ENV__Fault(struct soap *soap, const char *soap_endpoint, const char *soap_action, char *faultcode, char *faultstring, char *faultactor, struct SOAP_ENV__Detail *detail, struct SOAP_ENV__Code *SOAP_ENV__Code, struct SOAP_ENV__Reason *SOAP_ENV__Reason, char *SOAP_ENV__Node, char *SOAP_ENV__Role, struct SOAP_ENV__Detail *SOAP_ENV__Detail)
